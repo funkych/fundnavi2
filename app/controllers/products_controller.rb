d9d1520194e345a3632a8731d071d3fdbc3fef04
tech-camp <tech-camp@tech-camp-1-081-no-MacBook-Air-7.local>
2017-03-04 15:46:21 +0900

    productsコントローラー作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
 
-def index
-end
+  def index
+    # productsテーブルから最新順に作品を２０件取得する
+    @products = Product.order('id ASC').limit(20)
+  end
+
+  def show
+    # productsテーブルから該当するidの作品productの変数へ代入する処理を書いて下さい
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  end
 
 end
